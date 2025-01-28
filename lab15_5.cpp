a34caf0481316f53eaadc7acceb2805b0c0a5f58
Manoon666 <manoon8412@gmail.com>
2025-01-28 15:55:45 +0700

    lab15_5fished

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -7,7 +7,7 @@ void shuffle(int *,int *,int *,int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
-	
+	 
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
@@ -20,3 +20,17 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+void shuffle(int *a,int *b,int *c,int *d){
+    int x[4] = {*a, *b, *c, *d};
+	int temp;
+	int n = rand()%4;
+    for (int i = 0; i < 4; i++) {
+        temp = x[i];
+		x[i] = x[n];
+		x[n] = temp;
+ 	}
+	*a = x[0];
+	*b = x[1];
+	*c = x[2];
+	*d = x[3];
+}
